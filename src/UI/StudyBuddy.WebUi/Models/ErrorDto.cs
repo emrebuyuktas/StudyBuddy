3aea053586598403f699b7d98dfc59e46ac94dc1
emrebuyuktas <emrebyk348@gmail.com>
2022-11-23 19:45:36 +0300

    fix bug

--- src/UI/StudyBuddy.WebUi/Models/ErrorDto.cs
+++ src/UI/StudyBuddy.WebUi/Models/ErrorDto.cs
@@ -3,15 +3,4 @@
 public class ErrorDto
 {
     public List<String> Errors { get; private set; } = new List<string>();
-
-
-    public ErrorDto(string error)
-    {
-        Errors.Add(error);
-    }
-
-    public ErrorDto(List<string> errors)
-    {
-        Errors = errors;
-    }
 }
