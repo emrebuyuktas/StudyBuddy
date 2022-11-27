564f3f1deec157f24b4b29cdcf445818b69254ad
Kilicceker <omerkiliccekercs@gmail.com>
2022-11-27 14:34:54 +0300

    change signup

--- src/UI/StudyBuddy.WebUi/Models/SignupDto.cs
+++ src/UI/StudyBuddy.WebUi/Models/SignupDto.cs
@@ -5,4 +5,6 @@ public class SignupDto
     public string UserName { get; set; }
     public string Email { get; set; }
     public string Password { get; set; }
+    
+    public List<TagDto> TagDtos { get; set; }
 }
