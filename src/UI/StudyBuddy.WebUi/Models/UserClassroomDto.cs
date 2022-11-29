c8b3a9196260a7ee069ec4feb7c45b333ff05c59
Kilicceker <omerkiliccekercs@gmail.com>
2022-11-29 20:40:20 +0300

    add my class room

--- src/UI/StudyBuddy.WebUi/Models/UserClassroomDto.cs
+++ src/UI/StudyBuddy.WebUi/Models/UserClassroomDto.cs
@@ -2,6 +2,10 @@
 
 public class UserClassroomDto
 {
+    public string ClassroomId { get; set; }
+    
     public string UserName { get; set; }
     public string ClassroomName { get; set; }
+    public TagDto Tag { get; set; }
+    
 }
--- src/core/StudyBuddy.Application/Dtos/UserClassroomDto.cs
+++ src/core/StudyBuddy.Application/Dtos/UserClassroomDto.cs
@@ -5,6 +5,7 @@ namespace StudyBuddy.Application.Dtos;
 public class UserClassroomDto
 {
     public string ClassroomId { get; set; }
+    
     public string UserName { get; set; }
     public string ClassroomName { get; set; }
     public TagDto Tag { get; set; }
